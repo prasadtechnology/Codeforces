739a5f8d0a1bd66442422f9110f4b69ce1b7fa97
prasadarao <pulipati.pulipati.prasad4@gmail.com>
2017-01-28 08:57:03 +0530

    small changes

--- BeautifulMatrix.java
+++ BeautifulMatrix.java
@@ -1,16 +1,23 @@
-/**
-Bitcount
-**/
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.BufferedOutputStream;
 import java.io.PrintWriter;
-public class RaisingBacteria{
+public class BeautifulMatrix{
 	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	static PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
 	public static void main(String[] args) throws Exception{
-		int n = Integer.parseInt(in.readLine());
-		pw.println(Integer.bitCount(n));
+		int result = -1;
+		int row = 0;
+		int col = 0;
+		for(int i = 0; i < 5; i++){
+			String str = in.readLine();
+			String temp = str.replaceAll("\\s+","");
+			if(str.contains("1")){
+				col = temp.indexOf("1")+1;
+				row = i+1;
+			}
+		}
+		pw.println(Math.abs(3-(row)) + Math.abs(3-(col)));
 		pw.close();
 	}
 }
